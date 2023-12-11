29aff90256ab893bd511282eefcba523a94c423c
Hervé Kay <herve.kay@protonmail.com>
2023-12-11 22:37:00 +0000

    feat(day_1): finished day 1 trebuchet

--- day_1_trebuchet/src/main.rs
+++ day_1_trebuchet/src/main.rs
@@ -1,3 +1,24 @@
+use std::path::Path;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+    let file_path = Path::new("../data/day_1_input.txt");
+    println!("In file {}", file_path.display());
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    
+    let result = contents.lines()
+        .fold(0, |acc, line| {
+            let number = line.chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<Vec<_>>();
+            let first_number = number.first().map(|c| c.to_digit(10)).flatten().unwrap_or(0) as i32;
+            let last_number = number.last().map(|c| c.to_digit(10)).flatten().unwrap_or(0) as i32;
+            println!("Current line {line} - first number {first_number} - last number {last_number}");
+            acc + first_number * 10 + last_number
+        });
+    println!("With text:\n{result}");
 }
